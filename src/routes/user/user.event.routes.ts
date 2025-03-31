1d045492a90a4f40744c0202b9cd404a5e01070e
AroNicolas <hei.nicolas.5@gmail.com>
2025-03-31 19:37:27 +0300

    chore(user.event.routes): change paths order to avoid params problem

--- src/routes/user/user.event.routes.ts
+++ src/routes/user/user.event.routes.ts
@@ -3,9 +3,9 @@ import { EventController } from "../../controllers/user/user.event.controller";
 
 const router = Router();
 
-router.get("/event/:id_event", EventController.getEventById);
-router.get("/event", EventController.getAllOrFilteredEvents);
 router.get("/event/locations", EventController.getAllLocations);
 router.get("/event/categories", EventController.getAllCategories);
+router.get("/event/:id_event", EventController.getEventById);
+router.get("/event", EventController.getAllOrFilteredEvents);
 
 export default router;
